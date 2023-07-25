175ee92b9f6173149ef31aab23981edf27c35c29
Tiago Vieira <tiagosvieira10@gmail.com>
2023-07-25 13:38:35 -0300

    pagina de transaçao concluida

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,98 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header/>
+      <Summary/>
+
+      <TransactionsContainer>
+        <SearchForm/>
+
+        <TransactionsTable>
+        <tbody>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="incone">
+                R$ 12.000,00
+              </PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Hamburguer</td>
+            <td>
+              <PriceHighlight variant="outcone">
+                - R$ 59,00
+              </PriceHighlight>  
+            </td>
+            <td>Alimentação</td>
+            <td>10/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Aluguel do apartamento</td>
+            <td>
+              <PriceHighlight variant="outcone">
+                - R$ 1.200,00
+              </PriceHighlight>  
+            </td>
+            <td>Casa</td>
+            <td>13/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Computador</td>
+            <td>
+              <PriceHighlight variant="incone">
+                R$ 5.400,00
+              </PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="incone">
+                R$ 8.000,00
+              </PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Janta</td>
+            <td>
+              <PriceHighlight variant="outcone">
+                - R$ 39,00
+              </PriceHighlight>
+            </td>
+            <td>Alimentação</td>
+            <td>13/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="incone">
+                R$ 12.000,00
+              </PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+        </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
