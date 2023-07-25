eacee21f18508ef53842d7044d636b6ff4a41dbe
Tiago Vieira <tiagosvieira10@gmail.com>
2023-07-25 15:27:01 -0300

    modal New Transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
+import * as Dialog from '@radix-ui/react-dialog'
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -9,7 +10,14 @@ export function Header() {
       <img src={logoImg} alt="" />
 
 
-      <NewTransactionButton>Nova transação</NewTransactionButton>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova transação</NewTransactionButton>
+        </Dialog.Trigger>
+
+       <NewTransactionModal/>
+
+      </Dialog.Root>
     </HeaderContent>
    </HeaderContainer>
   )
