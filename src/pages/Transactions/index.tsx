96b056bc3fb4c0f19fe16725e01c6df80b141cb1
Tiago Vieira <tiagosvieira10@gmail.com>
2023-07-26 11:34:16 -0300

    adicionando api server

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'incone' | 'outcone';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions(){
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json();
+    
+      setTransactions(data)
+ }
+  useEffect(() => {
+    loadTransactions();
+  }, [])
+
   return (
     <div>
       <Header/>
@@ -14,82 +36,20 @@ export function Transactions() {
 
         <TransactionsTable>
         <tbody>
-          <tr>
-            <td width="50%">Desenvolvimento de site</td>
-            <td>
-              <PriceHighlight variant="incone">
-                R$ 12.000,00
-              </PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-
-          <tr>
-            <td width="50%">Hamburguer</td>
-            <td>
-              <PriceHighlight variant="outcone">
-                - R$ 59,00
-              </PriceHighlight>  
-            </td>
-            <td>Alimentação</td>
-            <td>10/04/2022</td>
-          </tr>
-
-          <tr>
-            <td width="50%">Aluguel do apartamento</td>
-            <td>
-              <PriceHighlight variant="outcone">
-                - R$ 1.200,00
-              </PriceHighlight>  
-            </td>
-            <td>Casa</td>
-            <td>13/04/2022</td>
-          </tr>
-
-          <tr>
-            <td width="50%">Computador</td>
-            <td>
-              <PriceHighlight variant="incone">
-                R$ 5.400,00
-              </PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-
-          <tr>
-            <td width="50%">Desenvolvimento de site</td>
-            <td>
-              <PriceHighlight variant="incone">
-                R$ 8.000,00
-              </PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-
-          <tr>
-            <td width="50%">Janta</td>
-            <td>
-              <PriceHighlight variant="outcone">
-                - R$ 39,00
-              </PriceHighlight>
-            </td>
-            <td>Alimentação</td>
-            <td>13/04/2022</td>
-          </tr>
-
-          <tr>
-            <td width="50%">Desenvolvimento de site</td>
-            <td>
-              <PriceHighlight variant="incone">
-                R$ 12.000,00
-              </PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
+          {transactions.map(transactions => {
+            return(
+              <tr key={transactions.id}>
+                <td width="50%">{transactions.description}</td>
+                <td>
+                  <PriceHighlight variant={transactions.type}>
+                    {transactions.price}
+                  </PriceHighlight>
+                </td>
+                <td>{transactions.category}</td>
+                <td>{transactions.createdAt}</td>
+              </tr>
+            )
+          })}
         </tbody>
         </TransactionsTable>
       </TransactionsContainer>
